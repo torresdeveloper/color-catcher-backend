8d17b1551aa61d5d2349cb8a78cf90eb68dcf158
Matheus Torres <matheustorres1201@gmail.com>
2023-08-23 16:45:59 -0400

    update

--- src/app.js
+++ src/app.js
@@ -1,68 +1,66 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
-const cors = require('cors'); 
-
+const express = require("express");
+const cors = require("cors");
+const scraper = require("./WebScraping");
 
 const app = express();
-
-app.use(cors()); 
-
+app.use(cors());
 app.use(express.json());
 
-app.get("/api" , async(req, res) => {
-    res.json({
-        status: "ok"
+app.get("/api", async (req, res) => {
+  res.json({
+    status: "ok",
+  });
+});
+
+app.post("/api/scrape", async (req, res) => {
+  const siteURL = req.body.url;
+
+  if (!siteURL) {
+    res.status(400).json({
+      error: "Please provide a URL to scrape",
     });
+    return;
+  }
+
+  try {
+    const browser = await scraper.getBrowser();
+    const page = await browser.newPage();
+
+    await page.goto(siteURL);
+
+    const colors = await page.evaluate(() => {
+      const elements = document.querySelectorAll("*");
+      const uniqueColors = new Set();
+
+      elements.forEach((element) => {
+        const backgroundColor = getComputedStyle(element).backgroundColor;
+        const color = getComputedStyle(element).color;
+
+        if (
+          backgroundColor &&
+          backgroundColor !== "rgba(0, 0, 0, 0)" &&
+          backgroundColor !== "transparent"
+        ) {
+          uniqueColors.add(backgroundColor);
+        }
+
+        if (color && color !== "rgb(0, 0, 0)") {
+          uniqueColors.add(color);
+        }
+      });
+
+      return Array.from(uniqueColors);
+    });
+
+    await page.close();
+
+    res.json({ colors });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: `An error occurred while scraping the website: ${error}`,
+    });
+  }
 });
-
-app.post('/api/scrape', async (req, res) => {
-    const siteURL = req.body.url;
-
-    try {
-        const browser = await puppeteer.launch();
-        // const browser = await chromium.puppeteer.launch({
-        //     args: chromium.args,
-        //     defaultViewport: chromium.defaultViewport,
-        //     executablePath: await chromium.executablePath,
-        //     //headless: chromium.headless,
-        //     headless: true,
-        //     ignoreHTTPSErrors: true,
-        //   });
-        const page = await browser.newPage();
-
-        await page.goto(siteURL);
-
-        const colors = await page.evaluate(() => {
-            const elements = document.querySelectorAll('*');
-            const uniqueColors = new Set();
-
-            elements.forEach(element => {
-                const backgroundColor = getComputedStyle(element).backgroundColor;
-                const color = getComputedStyle(element).color;
-
-                if (backgroundColor && backgroundColor !== 'rgba(0, 0, 0, 0)' && backgroundColor !== 'transparent') {
-                    uniqueColors.add(backgroundColor);
-                }
-
-                if (color && color !== 'rgb(0, 0, 0)') {
-                    uniqueColors.add(color);
-                }
-            });
-
-            return Array.from(uniqueColors);
-        });
-
-        await browser.close();
-
-        res.json({ colors });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred while scraping the website.' });
-    }
-});
-
-
 
 module.exports = app;
-
-
